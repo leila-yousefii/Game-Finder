f69de789eabf99fed491e79de9e293d5076be8d1
leila <leilayousefi.y7@gmail.com>
2023-05-29 01:07:19 +0330

    add loading skeletons

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -3,4 +3,4 @@ const index=url.indexOf('media/')+'media/'.length;
 return url.slice(0, index)+'crop/600/400/'+url.slice(index);
 
 }
-export default getCroppedImageUrl;
+export default getCroppedImageUrl; 
