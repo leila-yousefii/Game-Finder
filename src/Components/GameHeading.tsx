1448ab749f97d8cab2a51c603ec675ea894b8ca1
leila <leilayousefi.y7@gmail.com>
2023-06-05 16:19:59 +0330

    refactor: create lookup hooks

--- src/Components/GameHeading.tsx
+++ src/Components/GameHeading.tsx
@@ -1,8 +1,10 @@
 import { Heading } from '@chakra-ui/react'
 
 import usePlatforms, { Platform } from '../hooks/usePlatforms';
-import useGenres, { Genre } from '../hooks/useGenres';
+import useGenres from '../hooks/useGenres';
 import { GameQuery } from './../App';
+import usePlatform from '../hooks/usePlatform';
+import useGenre from './../hooks/useGenre';
 
 interface Props{
     gameQuery: GameQuery;
@@ -10,11 +12,8 @@ interface Props{
   }
 
 const GameHeading = ({gameQuery}:Props) => {
-  const {data: genres}=useGenres();
-  const genre= genres?.results.find(g=>g.id===gameQuery.genreId);
-
-  const {data:platforms}=usePlatforms();
-  const platform= platforms?.results.find(p=>p.id===gameQuery.platformId);
+ const genre=useGenre(gameQuery.genreId)
+  const platform= usePlatform(gameQuery.platformId);
 
     const heading=`${platform?.name|| ''} ${genre?.name || ''} Games`
   return (
